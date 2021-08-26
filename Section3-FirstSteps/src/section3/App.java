c34fbd663640e0e252a8f736a8a5a93b764e5108
joaokbmartins <martins.joao1909@gmail.com>
2021-08-25 23:51:49 -0300

    chore: ib to kg added

--- Section3-FirstSteps/src/section3/App.java
+++ Section3-FirstSteps/src/section3/App.java
@@ -6,23 +6,31 @@ public class App {
 
     byte byteMin = Byte.MIN_VALUE;
     byte byteMax = Byte.MAX_VALUE;
-    System.out.format("byteMin: %1d, byteMax: %2d\n", byteMin, byteMax);
+    System.out.printf("08 bits | byteMin: %1d, byteMax: %2d\n", byteMin, byteMax);
     short shortMin = Short.MIN_VALUE;
     short shortMax = Short.MAX_VALUE;
-    System.out.format("shortMin: %1d, shortMax: %2d\n", shortMin, shortMax);
+    System.out.printf("16 bits | shortMin: %1d, shortMax: %2d\n", shortMin, shortMax);
     int integerMin = Integer.MIN_VALUE;
     int integerMax = Integer.MAX_VALUE;
-    System.out.format("integerMin: %1d, integerMax: %2d\n", integerMin, integerMax);
+    System.out.printf("32 bits | intMin: %1d, intMax: %2d\n", integerMin, integerMax);
     long longMin = Long.MIN_VALUE;
     long longMax = Long.MAX_VALUE;
-    System.out.format("longMin: %1d, longMax: %2d\n", longMin, longMax);
+    System.out.printf("64 bits | longMin: %1d, longMax: %2d\n", longMin, longMax);
     float floatMin = Float.MIN_VALUE;
     float floatMax = Float.MAX_VALUE;
-    System.out.format("floatMin: %1f, floatMax: %2f\n", floatMin, floatMax);
+    System.out.printf("floatMin: %1f, floatMax: %2f\n", floatMin, floatMax);
     double doubleMin = Double.MIN_VALUE;
     double doubleMax = Double.MAX_VALUE;
-    System.out.format("doubleMin: %1f, doubleMax: %2f\n", doubleMin, doubleMax);
+    System.out.printf("doubleMin: %1f, doubleMax: %2f\n", doubleMin, doubleMax);
 
+    long maximus = ((long) byteMax + (long) shortMax + (long) integerMax);
+    System.out.println("maximus: " + maximus);
+    long times10 = maximus * 10L;
+    System.out.println("times10: " + times10);
+    long total = 50000L + times10;
+    System.out.println("Total  : " + total);
+
+    float var = 5.43F;
   }
 
 }
