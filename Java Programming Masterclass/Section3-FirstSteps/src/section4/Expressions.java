c46c75c5e233239952d334dda5bde976845ffdea
joaokbmartins <martins.joao1909@gmail.com>
2021-08-27 01:20:02 -0300

    chore: dealing with expressions and statements

---
+++ Java Programming Masterclass/Section3-FirstSteps/src/section4/Expressions.java
@@ -0,0 +1,27 @@
+package section4;
+
+public class Expressions {
+
+  public static void main(String[] args) {
+    //  Expressions:
+    //    > in variables declaration, the content between the type and the semicolon, ex:
+    //        String
+    //          name = "David" // expression
+    //        ;
+    //    > string literals between double quotes, including them, ex:
+    //        System.out.println(
+    //          "David" // expression
+    //        );
+    //    > content between parenthesis, ex:
+    //        if (
+    //          1 == 1 // expression
+    //        ){ }
+    //
+    //  Statements:
+    //    > the complete line, til the semicolon, ex:
+    //        String name = "David"; // statement
+    //        System.out.println("David"); // statement
+
+  }
+
+}
