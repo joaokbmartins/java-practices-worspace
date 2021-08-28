7b1540eaa3896c655e5c8fac0da3b668bcfa558d
joaokbmartins <martins.joao1909@gmail.com>
2021-08-28 02:47:06 -0300

    chore: megabytes converter

---
+++ Java Programming Masterclass/Section3-FirstSteps/src/section4/MegaBytesConverter.java
@@ -0,0 +1,18 @@
+package section4;
+
+public class MegaBytesConverter {
+  public static void main(String[] args) {
+    printMegaBytesAndKiloBytes(2500);
+    printMegaBytesAndKiloBytes(-1024);
+    printMegaBytesAndKiloBytes(5000);
+  }
+
+  public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+    if (kiloBytes < 0) {
+      System.out.println("Invalid Value");
+    } else {
+      int mb = kiloBytes / 1024;
+      System.out.println(kiloBytes + " KB = " + mb + " MB and " + (kiloBytes % 1024) + " KB");
+    }
+  }
+}
--- Java Programming Masterclass/Section3-FirstSteps/src/section4/SpeedConverter.java
+++ Java Programming Masterclass/Section3-FirstSteps/src/section4/SpeedConverter.java
@@ -3,23 +3,17 @@ package section4;
 public class SpeedConverter {
 
   public static void main(String[] args) {
-    Teste.printConversion(1.5);
-    Teste.printConversion(10.25);
-    Teste.printConversion(-10.25);
-    Teste.printConversion(25.42);
-    Teste.printConversion(75.114);
+    printConversion(1.5);
+    printConversion(10.25);
+    printConversion(-10.25);
+    printConversion(25.42);
+    printConversion(75.114);
   }
 
-}
-
-class Teste {
-
   public static long toMilesPerHour(double kilometersPerHour) {
-
     if (kilometersPerHour < 0) {
       return -1;
     }
-
     return Math.round(kilometersPerHour / 1.609d);
   }
 
