81e016166311e5823536b99ccabc371d2e25015c
joaokbmartins <martins.joao1909@gmail.com>
2021-09-01 23:33:25 -0300

    chore: mins years days converter

---
+++ Java Programming Masterclass/Section3-FirstSteps/src/section4/MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,21 @@
+package section4;
+
+public class MinutesToYearsAndDaysCalculator {
+
+  public static void main(String[] args) {
+    printYearsAndDays(525600);
+    printYearsAndDays(1051200);
+    printYearsAndDays(561600);
+  }
+
+  public static void printYearsAndDays(long minutes) {
+    if (minutes < 0) {
+      System.out.println("Invalid Value");
+    } else {
+      long years = minutes / 60 / 24 / 365;
+      long days = minutes / 60 / 24 % 365;
+      System.out.println(minutes + " min = " + years + " y and " + days + " d ");
+    }
+  }
+
+}
